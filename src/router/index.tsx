a6b15886ecb963ad5fce1e17191e0e2ece3cf1a9
Ojolowo <ojolowoboluwatife1@gmail.com>
2024-07-22 23:34:41 +0100

    Setup: Setup React Router

---
+++ src/router/index.tsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { appRoutes } from "./routes";
+
+const router = createBrowserRouter(appRoutes)
+
+export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+//@ts-expect-error
+import path from "path";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+//@ts-expect-error
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
 })
