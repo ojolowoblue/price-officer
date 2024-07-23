725e2b12b0b7c005202b04f99efabb6528f54156
Ojolowo <ojolowoboluwatife1@gmail.com>
2024-07-23 08:01:15 +0100

    configure app colors and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4A27B5",
+        "primary-light": "#F1EEFB",
+        background: "#FFF",
+        border: "#E5E7EB",
+        divider: "#F2F4F7"
+      },
+      keyframes: {
+        "full-width": { to: { width: "100%" } },
+        "fade-in": {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease",
+        preflight: "full-width 2s ease-in-out infinite",
+      },
+
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
