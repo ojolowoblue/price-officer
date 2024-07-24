28c5a5c771608734acfcc93921688e3a3f85d1cf
Ojolowo <ojolowoboluwatife1@gmail.com>
2024-07-25 00:56:20 +0100

    add app links

---
+++ src/constants/appLinks.ts
@@ -0,0 +1,7 @@
+export const appLinks = [
+  { name: 'Home', path: '/' },
+  { name: 'View Prices', path: '/view-prices' },
+  { name: 'Report Price', path: '/report-price' },
+  { name: 'Profile', path: '/profile' },
+  { name: 'About Price Police', path: '/about' },
+];
