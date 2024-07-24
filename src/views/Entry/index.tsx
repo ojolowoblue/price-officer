275fb5001a176edb27d9a03ee0b38bf4f82f854f
Ojolowo <ojolowoboluwatife1@gmail.com>
2024-07-25 00:57:13 +0100

    refactor

--- src/views/Entry/index.tsx
+++ src/views/Entry/index.tsx
@@ -1,44 +1,7 @@
 import SearchBox from '@/components/ui/Search';
 
-import PriceCard, { Price } from './components/PriceCard';
-
-const priceList: Price[] = [
-  {
-    name: 'Smoked Eja Abo ( ukpek fish)',
-    location: 'Lekki, Lagos',
-    updatedAt: new Date().toISOString(),
-    image: '/images/eja.png',
-    price: 2500,
-  },
-  {
-    name: 'Cocoa',
-    location: 'Lekki, Lagos',
-    updatedAt: new Date().toISOString(),
-    image: '/images/cocoa.png',
-    price: 5480,
-  },
-  {
-    name: 'Golden Penny Spaghetti 500g',
-    location: 'Lekki, Lagos',
-    updatedAt: new Date().toISOString(),
-    image: '/images/spaghetti.png',
-    price: 890,
-  },
-  {
-    name: 'Ofada Rice, 1 derica',
-    location: 'Lekki, Lagos',
-    updatedAt: new Date().toISOString(),
-    image: '/images/ofada.png',
-    price: 3000,
-  },
-  {
-    name: 'Plain baking flour (half paint)',
-    location: 'Lekki, Lagos',
-    updatedAt: new Date().toISOString(),
-    image: '/images/flour.png',
-    price: 3500,
-  },
-];
+import { priceList } from '@/constants/data';
+import PriceCard from './components/PriceCard';
 
 export default function Entry() {
   return (
