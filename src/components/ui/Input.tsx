7d7b8e3d1ed8e514dfd9d91bd5afe7146bb209cf
Ojolowo <ojolowoboluwatife1@gmail.com>
2024-07-26 12:53:33 +0100

    add comment section

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -12,12 +12,16 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
     <div>
       {label && <Label className="mb-2.5">{label}</Label>}
 
-      <div className="flex items-center px-5 h-11 w-full rounded-md border border-input bg-background placeholder:font-normal text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-placeholder focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2">
+      <div
+        className={cn(
+          'flex items-center px-5 h-11 w-full rounded-md border border-input bg-background placeholder:font-normal text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-placeholder focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2',
+          className,
+        )}
+      >
         <input
           type={type}
           className={cn(
             'h-full w-full bg-transparent py-2 outline-none text-base focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-            className,
           )}
           ref={ref}
           {...props}
